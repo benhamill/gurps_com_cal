485071dd5a8db99170d4aec539b5bfc8dc21727e
Ben Hamill <git-commits@benhamill.com>
2011-01-27 00:42:51 -0600

    Secondary characteristics.

--- lib/gurps_com_cal/character/attributes.rb
+++ lib/gurps_com_cal/character/attributes.rb
@@ -1,6 +1,7 @@
 module GurpsComCal
   class Character
     module Attributes
+      # Basic Attributes
       attr_writer :st, :dx, :iq, :ht
 
       def st
@@ -18,6 +19,72 @@ module GurpsComCal
       def ht
         @ht || 10
       end
+
+      # Secondary Characteristics
+      attr_writer :thrust, :swing, :bl, :hp, :will, :per :fp, :basic_speed, :basic_move
+
+      def thrust
+        @thrust || DAMAGE_TABLE[st][:thr]
+      end
+
+      def swing
+        @swing || DAMAGE_TABLE[st][:sw]
+      end
+
+      def bl
+        @bl || st * st / 5.0
+      end
+
+      def hp
+        @hp || st
+      end
+
+      def will
+        @will || iq
+      end
+
+      def per
+        @per || iq
+      end
+
+      def fp
+        @fp || ht
+      end
+
+      def basic_speed
+        @basic_speed || (ht + dx) / 4.0
+      end
+
+      def basic_move
+        @basic_move || basic_speed.to_i
+      end
+
+      def dodge
+        @dodge || basic_speed.to_i + 3
+      end
+
+      DAMAGE_TABLE = {
+        1   =>  {:thr => '1d-6' , :sw => '1d-5' },
+        2   =>  {:thr => '1d-6' , :sw => '1d-5' },
+        3   =>  {:thr => '1d-5' , :sw => '1d-4' },
+        4   =>  {:thr => '1d-5' , :sw => '1d-4' },
+        5   =>  {:thr => '1d-4' , :sw => '1d-3' },
+        6   =>  {:thr => '1d-4' , :sw => '1d-3' },
+        7   =>  {:thr => '1d-3' , :sw => '1d-2' },
+        8   =>  {:thr => '1d-3' , :sw => '1d-2' },
+        9   =>  {:thr => '1d-2' , :sw => '1d-1' },
+        10  =>  {:thr => '1d-2' , :sw => '1d'   },
+        11  =>  {:thr => '1d-1' , :sw => '1d+1' },
+        12  =>  {:thr => '1d-1' , :sw => '1d+2' },
+        13  =>  {:thr => '1d'   , :sw => '2d-1' },
+        14  =>  {:thr => '1d'   , :sw => '2d'   },
+        15  =>  {:thr => '1d+1' , :sw => '2d+1' },
+        16  =>  {:thr => '1d+1' , :sw => '2d+2' },
+        17  =>  {:thr => '1d+2' , :sw => '3d-1' },
+        18  =>  {:thr => '1d+2' , :sw => '3d'   },
+        19  =>  {:thr => '2d-1' , :sw => '3d+1' },
+        20  =>  {:thr => '2d-1' , :sw => '3d+2' },
+      }
     end
   end
 end
