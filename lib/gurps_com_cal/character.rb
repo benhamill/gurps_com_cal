9fa65cf1736d764edc730dda6fa3c12b8d634414
Ben Hamill <git-commits@benhamill.com>
2011-01-27 01:20:01 -0600

    Convert dice strings around to handle modifiers.

--- lib/gurps_com_cal/character.rb
+++ lib/gurps_com_cal/character.rb
@@ -6,16 +6,40 @@ module GurpsComCal
     include Attributes
     include Yaml
 
-    attr_writer :name
-
     def initialize options = {}
       options.each_key do |key|
         self.send("#{key}=", options[key]) if self.respond_to? "#{key}="
       end
     end
 
+    attr_writer :name
+
     def name
       @name || 'Goon'
+    end
+
+    def dice_math *args
+      dice_hash = args.inject({:dice => 0, :adds => 0}) do |hash, dice_string|
+        if dice_string =~ /d/
+          dice, adds = dice_string.split('d')
+          hash[:dice] += dice.to_i
+          hash[:adds] += adds.to_i
+        else
+          hash[:adds] += dice_string.to_i
+        end
+
+        hash
+      end
+
+      if dice_hash[:adds] < 0
+        adds = dice_hash[:adds].to_s
+      elsif dice_hash[:adds] > 0
+        adds = "+#{dice_hash[:adds]}"
+      else
+        adds = ''
+      end
+
+      "#{dice_hash[:dice]}d#{adds}"
     end
   end
 end
