4191d7fff676384a29d93eb463252483bede4aac
Ben Hamill <git-commits@benhamill.com>
2011-01-27 23:51:49 -0600

    Add equality checking.

--- lib/gurps_com_cal/roll.rb
+++ lib/gurps_com_cal/roll.rb
@@ -13,6 +13,14 @@ module GurpsComCal
       @adds = adds.to_i
     end
 
+    def == other
+      if other.respond_to?(:dice) and other.respond_to?(:adds)
+        self.dice == other.dice && self.adds == other.adds
+      else
+        false
+      end
+    end
+
     def + other
       if other.respond_to?(:dice) and other.respond_to?(:adds)
         Roll.new(self.dice + other.dice, self.adds + other.adds)
