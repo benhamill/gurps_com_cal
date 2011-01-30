6e319064484ac38120c245bc2017f8b186de2351
Ben Hamill <git-commits@benhamill.com>
2011-01-29 21:35:43 -0600

    Passing tests.

--- lib/gurps_com_cal/combatant.rb
+++ lib/gurps_com_cal/combatant.rb
@@ -14,7 +14,8 @@ module GurpsComCal
 
     def damage type, basic_damage
       @injury ||= 0
-      @injury += basic_damage
+      injury = basic_damage * WOUNDING_MODS[type.to_s]
+      @injury += injury
     end
 
     def current_hp
@@ -26,5 +27,19 @@ module GurpsComCal
     def pass_to_character? method
       @character.respond_to? method
     end
+
+    WOUNDING_MODS = {
+      'pi-' => 0.5,
+      'burn' => 1.0,
+      'cor' => 1.0,
+      'cr' => 1.0,
+      'fat' => 1.0,
+      'pi' => 1.0,
+      'tox' => 1.0,
+      'cut' => 1.5,
+      'pi+' => 1.5,
+      'imp' => 2.0,
+      'pi++' => 2.0
+    }
   end
 end
