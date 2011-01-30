db061d5272c576f1b77703e4522245058850cd5e
Ben Hamill <git-commits@benhamill.com>
2011-01-29 18:30:37 -0600

    Passing test.

--- lib/gurps_com_cal/combatant.rb
+++ lib/gurps_com_cal/combatant.rb
@@ -3,5 +3,9 @@ module GurpsComCal
     def initialize character
       @character = character
     end
+
+    def method_missing method, *args, &block
+      @character.send(method, args)
+    end
   end
 end
