1ae5768ae35877e889e119fcfca949c0ac6689fe
Ben Hamill <git-commits@benhamill.com>
2011-01-29 21:21:51 -0600

    Some tests passing.

--- lib/gurps_com_cal/combatant.rb
+++ lib/gurps_com_cal/combatant.rb
@@ -5,7 +5,17 @@ module GurpsComCal
     end
 
     def method_missing method, *args, &block
-      @character.send(method, *args, &block)
+      if pass_to_character? method
+        @character.send(method, *args, &block)
+      else
+        super(method, *args, &block)
+      end
+    end
+
+    private
+
+    def pass_to_character? method
+      @character.respond_to? method
     end
   end
 end
