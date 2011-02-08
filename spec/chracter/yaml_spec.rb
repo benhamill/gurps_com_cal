c24bb8728e51fa095ed855ecba60c2c48c488bd6
Ben Hamill <git-commits@benhamill.com>
2011-02-08 15:57:56 -0600

    Some specs for Character::Yaml#to_hash.

--- spec/chracter/yaml_spec.rb
+++ spec/chracter/yaml_spec.rb
@@ -5,9 +5,9 @@ class CharacterYamlHolder
 end
 
 describe "GurpsComCal::Character::Yaml" do
-  subject { CharacterYamlHolder.new }
+  context "class methods" do
+    subject { CharacterYamlHolder }
 
-  context "loading a yaml file" do
     before(:each) do
       yaml = <<-YAML
         ---
@@ -31,8 +31,42 @@ describe "GurpsComCal::Character::Yaml" do
     end
 
     it "should parse a yaml file and hand it to new" do
-      CharacterYamlHolder.should_receive(:new).with({'name' => 'Eris', 'epithets' => ['Discordia', 'Strife']})
-      CharacterYamlHolder.from_yaml(@file_location)
+      subject.should_receive(:new).with({'name' => 'Eris', 'epithets' => ['Discordia', 'Strife']})
+      subject.from_yaml(@file_location)
+    end
+  end
+
+  context "instance methods" do
+    subject { CharacterYamlHolder.new }
+
+    describe "to_hash" do
+      it "should convert the method to a hash of the instance variables" do
+        subject.instance_variable_set '@foo', 'foo'
+        subject.instance_variable_set '@bar', 'bar'
+        subject.instance_variable_set '@baz', 'baz'
+
+        subject.to_hash.should == { 'foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz' }
+      end
+
+      it "should handle a special case for the instance variable 'weapons'" do
+        weapons = {:a => double, :b => double, :c => double}
+
+        subject.instance_variable_set '@foo', 'foo'
+        subject.instance_variable_set '@weapons', weapons
+
+        weapons.each_with_index do |weapon, i|
+          weapon[1].should_receive(:to_hash) { { 'number' => i } }
+        end
+
+        subject.to_hash.should == {
+          'foo' => 'foo',
+          'weapons' => [
+            { 'number' => 0 },
+            { 'number' => 1 },
+            { 'number' => 2 }
+          ]
+        }
+      end
     end
   end
 end
