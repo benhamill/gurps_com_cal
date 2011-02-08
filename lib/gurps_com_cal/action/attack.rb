0a2ac7e326fc6aa2f8d468a3afe5718dd6be892f
Ben Hamill <git-commits@benhamill.com>
2011-02-08 03:07:46 -0600

    First pass at managing the attack action.

--- lib/gurps_com_cal/action/attack.rb
+++ lib/gurps_com_cal/action/attack.rb
@@ -1,4 +1,64 @@
 module GurpsComCal
   class Attack
+    def initialize actor, target
+      @actor = actor
+      @target = target
+    end
+
+    def start
+      [:select_weapon, "#{@actor.name}, select a weapon.", @actor.weapons]
+    end
+
+    def select_weapon name
+      @weapon = @actor.weapon(name)
+      [:select_attack, "#{@actor.name}, select an attack.", @weapon.attacks]
+    end
+
+    def select_attack name
+      @attack = @weapon.attack(name)
+      [:do_attack, "#{@actor.name} roll against #{@attack.skill} and enter the result.", nil]
+    end
+
+    def do_attack result
+      if result <= @attack.skill
+        [:select_defense, "Success! #{@target.name}, select a defense.", %w(parry dodge)]
+      else
+        false
+      end
+    end
+
+    def select_defense name
+      case name
+      when 'dodge'
+        @defense_target = @target.dodge
+        [:do_defense, "#{@target.name}, roll against #{@defense_target} and enter the result.", nil]
+      when 'parry'
+        [:select_parry_weapon, "#{@target.name}, select a weapon to parry with.", @target.weapons]
+      end
+    end
+
+    def select_parry_weapon name
+      @parry_weapon = @target.weapon(name)
+      [:select_parry_attack, "#{@target.name}, select an attack to parry with.", @parry_weapon.attacks]
+    end
+
+    def select_parry_attack name
+      @parry_attack = @parry_weapon.attack name
+      @defense_target = @parry_attack.parry
+      [:do_defense, "#{@target.name}, roll against #{@defense_target} and enter the result.", nil]
+    end
+
+    def do_defense result
+      if result <= @defense_target
+        false
+      else
+        [:do_damage, "Hit! #{@actor.name}, roll #{@attack.damage.roll} for damage and enter the result.", nil]
+      end
+    end
+
+    def do_damage result
+      injury = @target.damage(@attack.damage.type, result)
+      [nil, "#{@target.name} took #{injury} points of injury.", nil]
+    end
   end
 end
