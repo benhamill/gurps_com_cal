ad21bc6e550281bc85351cfb75bf04d790167f1f
Ben Hamill <git-commits@benhamill.com>
2011-02-09 00:58:56 -0600

    Tests to cover GCC::Character::Skills.

--- spec/chracter/skill_spec.rb
+++ spec/chracter/skill_spec.rb
@@ -1,4 +1,4 @@
-require 'gurps_comcal/character/skills'
+require 'gurps_com_cal/character/skills'
 
 class SkillsHolder
   include GurpsComCal::Character::Skills
@@ -6,4 +6,29 @@ end
 
 describe "GurpsComCal::Character::Skills" do
   subject { SkillsHolder.new }
+
+  before(:each) do
+    GurpsComCal::Character::Skill.stub(:from_hash) do |arg|
+      arg['name']
+    end
+
+    skill_list = [
+      { 'name' => "Fightin'" },
+      { 'name' => "Spittin'" },
+      { 'name' => "Shootin'" },
+    ]
+    subject.skills = skill_list
+  end
+
+  it "should assign skills as an array of objects" do
+    subject.instance_variable_get('@skills').should == {"Fightin'"=>"Fightin'", "Spittin'"=>"Spittin'", "Shootin'"=>"Shootin'"}
+  end
+
+  it "should return a list of skill names" do
+    subject.skills.should == %w{Fightin' Spittin' Shootin'}
+  end
+
+  it "should retrieve a skill by name" do
+    subject.skill("Fightin'").should == "Fightin'"
+  end
 end
