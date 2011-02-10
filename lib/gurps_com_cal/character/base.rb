852b06264260cc3784b124e18f6e1d89ee9c0401
Ben Hamill <git-commits@benhamill.com>
2011-02-09 22:23:32 -0600

    Uh. Right. Name the module.

--- lib/gurps_com_cal/character/base.rb
+++ lib/gurps_com_cal/character/base.rb
@@ -1,23 +1,25 @@
 module GurpsComCal
   class Character
-    def initialize options = {}
-      options.each_key do |key|
-        self.send("#{key}=", options[key]) if self.respond_to? "#{key}="
+    module Base
+      def initialize options = {}
+        options.each_key do |key|
+          self.send("#{key}=", options[key]) if self.respond_to? "#{key}="
+        end
       end
-    end
 
-    def inspect
-      instance_vars = instance_variables.collect do |var|
-        "#{var.to_s}=#{instance_variable_get(var).inspect}"
-      end.join(' ')
+      def inspect
+        instance_vars = instance_variables.collect do |var|
+          "#{var.to_s}=#{instance_variable_get(var).inspect}"
+        end.join(' ')
 
-      "#<#{self.class} #{instance_vars}>"
-    end
+        "#<#{self.class} #{instance_vars}>"
+      end
 
-    attr_writer :name
+      attr_writer :name
 
-    def name
-      @name || 'Goon'
+      def name
+        @name || 'Goon'
+      end
     end
   end
 end
