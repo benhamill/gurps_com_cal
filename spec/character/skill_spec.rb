15d0e493d2d7e63587b1db6b4af5b32f61f8d48a
Ben Hamill <git-commits@benhamill.com>
2011-02-09 22:51:58 -0600

    Specs for GCC::Character::Skill.

--- spec/character/skill_spec.rb
+++ spec/character/skill_spec.rb
@@ -23,5 +23,64 @@ describe GurpsComCal::Character::Skill do
         s.instance_variable_get('@modifier').should == -2
       end
     end
+
+    describe "from_hash" do
+      it "should pull out character, name and level from the hash" do
+        char = double
+        hash = {
+          'character' => char,
+          'name' => "Fightin'",
+          'level' => 'DX-1'
+        }
+
+        subject.should_receive(:new).with(char, "Fightin'", 'DX-1')
+        subject.from_hash(hash)
+      end
+
+      it "should ignore extraneous values in the hash" do
+        char = double
+        hash = {
+          'character' => char,
+          'name' => "Fightin'",
+          'level' => 'DX-1',
+          'awesomeness' => 10_000_000_000,
+          'color' => [:black, :blue, :red_all_over]
+        }
+
+        subject.should_receive(:new).with(char, "Fightin'", 'DX-1')
+        subject.from_hash(hash)
+      end
+    end
+  end
+
+  context "instance methods" do
+    subject { GurpsComCal::Character::Skill.new(@character, "Fightin'", 'DX', -2) }
+
+    before(:each) do
+      @character = double
+    end
+
+    describe "inspect" do
+      it "should return a string, because, really, I don't care" do
+        subject.inspect.should be_a(String)
+      end
+    end
+
+    describe "to_hash" do
+      it "should return the name in a hash" do
+        subject.to_hash['name'].should == "Fightin'"
+      end
+
+      it "should calculate the level into a string from the attribute and modifier" do
+        subject.to_hash['level'].should == "DX-2"
+      end
+    end
+
+    describe "level" do
+      it "should ask the character for the relevant attribute and apply the modifier" do
+        @character.should_receive(:dx).and_return(10)
+        subject.level.should == 8
+      end
+    end
   end
 end
