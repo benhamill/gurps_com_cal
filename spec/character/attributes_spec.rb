eeb4dbaffbf5def4a0ca50cacc954fc61a8e97a2
Ben Hamill <git-commits@benhamill.com>
2011-02-09 23:54:30 -0600

    Specs for damage attrs.

--- spec/character/attributes_spec.rb
+++ spec/character/attributes_spec.rb
@@ -29,4 +29,46 @@ describe "GurpsComCal::Character::Attributes" do
       subject.ht.should == 11
     end
   end
+
+  context "Secondary Characteristics" do
+    describe "damage" do
+      before(:all) do
+        subject.st = 12
+      end
+
+      describe "thrust" do
+        it "should default to the appropriate thurst value for ST on the Damage Table" do
+          GurpsComCal::Character::Attributes::DAMAGE_TABLE.should_receive('[]').with(12).and_return({ :thr => '1d-1' })
+          subject.thrust.should == '1d-1'
+        end
+
+        it "should store a value if set" do
+          subject.thrust = 'foobar'
+          subject.thrust.should == 'foobar'
+        end
+
+        it "should should be aliased as thr" do
+          subject.should_receive(:thrust)
+          subject.thr
+        end
+      end
+
+      describe "swing" do
+        it "should default to the appropriate thurst value for ST on the Damage Table" do
+          GurpsComCal::Character::Attributes::DAMAGE_TABLE.should_receive('[]').with(12).and_return({ :sw => '1d+2' })
+          subject.swing.should == '1d+2'
+        end
+
+        it "should store a value if set" do
+          subject.swing = 'foobar'
+          subject.swing.should == 'foobar'
+        end
+
+        it "should should be aliased as sw" do
+          subject.should_receive(:swing)
+          subject.sw
+        end
+      end
+    end
+  end
 end
