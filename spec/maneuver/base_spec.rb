1a2bef911df5c6398983d4f984a82dd8a0186704
Ben Hamill <git-commits@benhamill.com>
2011-02-14 23:40:05 -0600

    red.

--- spec/maneuver/base_spec.rb
+++ spec/maneuver/base_spec.rb
@@ -4,12 +4,19 @@ describe "GurpsComCal::Maneuver::Base" do
   subject { GurpsComCal::Maneuver::Base.new @character }
 
   before(:each) do
-    @character= double
+    @character = double
   end
 
   describe "initialization" do
     it "should take an actor" do
       subject.actor.should == @character
+    end
+  end
+
+  describe "#next" do
+    it "should invoke the #start when first called" do
+      subject.should_receive(:start)
+      subject.next
     end
   end
 end
