2a933f3ce285c94b37bba086b5e9a1bd88c96702
Ben Hamill <git-commits@benhamill.com>
2011-02-15 23:19:40 -0600

    Empty spec for GCC::Maneuver::Attack.

---
+++ spec/maneuver/attack_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper'
+
+describe "GurpsComCal::Maneuver::Attack" do
+  subject { GurpsComCal::Maneuver::Attack.new @character }
+
+  before(:all) do
+    @character = double
+  end
+end
