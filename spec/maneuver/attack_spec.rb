c863f58052760746a5e8c797abd8ea78cc75b3a4
Ben Hamill <git-commits@benhamill.com>
2011-02-16 01:23:16 -0600

    Failing spec about attack.

--- spec/maneuver/attack_spec.rb
+++ spec/maneuver/attack_spec.rb
@@ -4,6 +4,68 @@ describe "GurpsComCal::Maneuver::Attack" do
   subject { GurpsComCal::Maneuver::Attack.new @character }
 
   before(:all) do
-    @character = double
+    file = File.join(SPEC_WORKING_DIR, 'rick_castle.yaml')
+    File.delete(file) if File.exists?(file)
+
+    File.open(file, 'w') do |f|
+      f.puts <<-YAML
+        ---
+        name: Rick Castle
+        iq: 12
+        dx: 11
+        ht: 11
+        skills:
+          - name: Brawling
+            level: DX
+          - name: Knife
+            level: DX+1
+        weapons:
+          - name: Fist
+            weight: 0
+            attacks:
+              - name: Punch
+                type: melee
+                damage: thr-1 cr
+                skills:
+                  - Boxing
+                  - Brawling
+                  - Karate
+                  - DX
+                reach:
+                  - C
+                min_st: 0
+                parry_modifier: 0
+          - name: Large Knife
+            weight: 1
+            attacks:
+              - name: Swing
+                type: melee
+                damage: sw-2 cut
+                skills:
+                  - Knife
+                reach:
+                  - C
+                  - 1
+                min_st: 6
+                parry_modifier: -1
+              - name: Thrust
+                type: melee
+                damage: thr imp
+                skills:
+                  - Knife
+                reach:
+                  - C
+                min_st: 6
+                parry_modifier: -1
+      YAML
+    end
+
+    @character = GurpsComCal::Character.load_yaml file
+  end
+
+  it "should start off with asking for a target" do
+    subject.next.should be_true
+    subject.message.should == "Rick Castle, select a target."
+    subject.options.should == nil
   end
 end
