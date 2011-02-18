0b9a6994244fa55120555ceb5c38fdd8ec6081fa
Ben Hamill <git-commits@benhamill.com>
2011-02-17 23:20:33 -0600

    Restructure how these characters are loaded entirely.

--- spec/maneuver/attack_spec.rb
+++ spec/maneuver/attack_spec.rb
@@ -1,71 +1,9 @@
 require_relative '../spec_helper'
 
 describe "GurpsComCal::Maneuver::Attack" do
-  before(:all) do
-    @file = File.join(SPEC_WORKING_DIR, 'rick_castle.yaml')
-    File.delete(@file) if File.exists?(@file)
-
-    File.open(@file, 'w') do |f|
-      f.puts <<-YAML
-        ---
-        name: Rick Castle
-        iq: 12
-        dx: 11
-        ht: 11
-        skills:
-          - name: Brawling
-            level: DX
-          - name: Knife
-            level: DX+1
-        weapons:
-          - name: Fist
-            weight: 0
-            attacks:
-              - name: Punch
-                type: melee
-                damage: thr-1 cr
-                skills:
-                  - Boxing
-                  - Brawling
-                  - Karate
-                  - DX
-                reach:
-                  - C
-                min_st: 0
-                parry_modifier: 0
-          - name: Large Knife
-            weight: 1
-            attacks:
-              - name: Swing
-                type: melee
-                damage: sw-2 cut
-                skills:
-                  - Knife
-                reach:
-                  - C
-                  - 1
-                min_st: 6
-                parry_modifier: -1
-              - name: Thrust
-                type: melee
-                damage: thr imp
-                skills:
-                  - Knife
-                reach:
-                  - C
-                min_st: 6
-                parry_modifier: -1
-      YAML
-    end
-  end
-
   before(:each) do
-    @attacker = GurpsComCal::Character.load_yaml @file
-    @thug = GurpsComCal::Character.load_yaml @file
-    @thug.name = 'Thug'
-    @thug.st = 12
-    @thug.iq = 9
-    @thug.dx = 10
+    @attacker = GurpsComCal::Character.load_yaml File.join(SPEC_RESOURCES_DIR, 'rick_castle.yaml')
+    @thug = GurpsComCal::Character.load_yaml File.join(SPEC_RESOURCES_DIR, 'thug.yaml')
 
     @defense = double(:fail? => true, :continue? => false, :success? => false, :message => 'message from defense', :options => 'options from defense')
     @defense.stub(:next) { @defense }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ end
 require 'gurps_com_cal'
 
 SPEC_WORKING_DIR = File.join(File.expand_path('..', __FILE__), 'work')
+SPEC_RESOURCES_DIR = File.join(File.expand_path('..', __FILE__), 'resources')
 
 RSpec.configure do |config|
   config.before(:suite) do
