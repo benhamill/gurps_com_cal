bb2234113d90f8a09e16222f88b544730b20d932
Ben Hamill <git-commits@benhamill.com>
2011-02-17 22:56:14 -0600

    Test through defense interaction, more or less.

--- spec/maneuver/attack_spec.rb
+++ spec/maneuver/attack_spec.rb
@@ -64,6 +64,10 @@ describe "GurpsComCal::Maneuver::Attack" do
     @thug.st = 12
     @thug.iq = 9
     @thug.dx = 10
+
+    @defense = double(:fail? => true, :continue? => false, :success? => false, :message => 'message from defense', :options => 'options from defense')
+    @defense.stub(:next) { @defense }
+    GurpsComCal::Maneuver::Defense.stub(:new) { @defense }
   end
 
   subject { GurpsComCal::Maneuver::Attack.new @attacker }
@@ -72,6 +76,7 @@ describe "GurpsComCal::Maneuver::Attack" do
     subject.next
     subject.message.should == "Rick Castle, select a target."
     subject.options.should == nil
+    subject.continue?.should be_true
   end
 
   context "after selecting a target" do
@@ -82,6 +87,7 @@ describe "GurpsComCal::Maneuver::Attack" do
     it "should ask for a weapon to use" do
       subject.message.should == "Rick Castle, select a weapon."
       subject.options.sort.should == ['Fist', 'Large Knife'].sort
+      subject.continue?.should be_true
     end
   end
 
@@ -93,6 +99,7 @@ describe "GurpsComCal::Maneuver::Attack" do
     it "should ask which attack to use" do
       subject.message.should == 'Rick Castle, select an attack.'
       subject.options.should == ['Punch']
+      subject.continue?.should be_true
     end
   end
 
@@ -107,33 +114,80 @@ describe "GurpsComCal::Maneuver::Attack" do
     end
   end
 
-  context "after failing the attack roll" do
-    it "should end the maneuver" do
-      subject.next.next(@thug).next('Fist').next('Punch').next(15).should be_false
+  context "when failing the attack roll" do
+    before(:each) do
+      subject.next.next(@thug).next('Fist').next('Punch').next(15)
+    end
+
+    it "should tell about a miss" do
       subject.message.should == 'A miss! Maneuver ended.'
       subject.options.should == nil
-      subject.next.should be_false
+    end
+
+    it "should end the maneuver with a failure" do
+      subject.continue?.should be_false
+      subject.fail?.should be_true
+      subject.success?.should be_false
     end
   end
 
-  context "after making the attack roll" do
-    before(:each) do
+  context "when making the attack roll" do
+    after(:each) do
       subject.next.next(@thug).next('Fist').next('Punch').next(9)
     end
 
-    context "when the defender can defend" do
-      it "should ask the defender to select a defense" do
-        subject.message.should == 'Thug, select a defense.'
-        subject.options.sort.should == ['Parry', 'Dodge'].sort
-      end
-    end
-
-    context "when the defender can't defend" do
-      pending "Needs more design thought."
+    it "should delegate control to a defence" do
+      GurpsComCal::Maneuver::Defense.should_receive(:new) { @defense }
+      @defense.should_receive(:next)
+      subject.message.should == 'message from defense'
+      subject.options.should == 'options from defense'
     end
   end
 
-  context "after the defender chooses a defense" do
-    pending "Oh man, do I need to think about defense machinery."
+  context "while the defense is not yet complete" do
+    before(:each) do
+      @defense.stub(:continue?) { true }
+      @defense.stub(:fail?) { false }
+      subject.next.next(@thug).next('Fist').next('Punch').next(9).next
+    end
+
+    it "should have the message and options from the defense" do
+      subject.message.should == 'message from defense'
+      subject.options.should == 'options from defense'
+    end
+
+    it "should continue to delegate while the defense is going" do
+      subject.next
+      subject.message.should == 'message from defense'
+      subject.options.should == 'options from defense'
+    end
+  end
+
+  context "when the defense succeeds" do
+    before(:each) do
+      @defense.stub(:fail?) { false }
+      @defense.stub(:success?) { true }
+      subject.next.next(@thug).next('Fist').next('Punch').next(9)
+    end
+
+    it "should stop delegating" do
+      @defense.should_not_receive(:next)
+      subject.next
+    end
+
+    it "should tell have the message and options form the defense" do
+      subject.message.should == 'message from defense'
+      subject.options.should == 'options from defense'
+    end
+
+    it "should end the action with a failure" do
+      subject.continue?.should be_false
+      subject.fail?.should be_true
+      subject.success?.should be_false
+    end
+  end
+
+  context "when the defense fails" do
+    pending
   end
 end
