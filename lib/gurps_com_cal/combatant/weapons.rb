15ccaa11f35e0258f5c99100648d75668d380656
Ben Hamill <git-commits@benhamill.com>
2011-02-24 16:43:47 -0600

    Equip weapons.

--- lib/gurps_com_cal/combatant/weapons.rb
+++ lib/gurps_com_cal/combatant/weapons.rb
@@ -1,6 +1,15 @@
 module GurpsComCal
   class Combatant
     module Weapons
+      attr_reader :equipped_weapon
+
+      def equip_weapon weapon
+        if @character.weapon(weapon.name) == weapon
+          @equipped_weapon = @character.weapon(weapon.name)
+        end
+
+        self
+      end
     end
   end
 end
--- spec/combatant/weapons_spec.rb
+++ spec/combatant/weapons_spec.rb
@@ -5,7 +5,29 @@ class WeaponsHolder
 end
 
 describe "GurpsComCal::Combatant::Weapons" do
+  before(:each) do
+    @character = double(GurpsComCal::Character)
+    subject.instance_variable_set('@character', @character)
+  end
+
   subject { WeaponsHolder.new }
 
-  pending
+  describe "#equip_weapon" do
+    before(:each) do
+      @weapon = double(GurpsComCal::Weapon, :name => 'Fist')
+      @character.stub(:weapon).with('Fist') { @weapon }
+    end
+
+    context "with a weapon the character owns" do
+      before(:each) do
+        @arg_weapon = double(GurpsComCal::Weapon, :name => 'Fist')
+        @weapon.stub(:==).with(@arg_weapon) { true }
+      end
+
+      it "should accept a weapon as argument" do
+        subject.equip_weapon(@arg_weapon)
+        subject.equipped_weapon.should == @weapon
+      end
+    end
+  end
 end
