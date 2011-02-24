9a9521c8c51553a37224f91ad25920edf19a5e92
Ben Hamill <git-commits@benhamill.com>
2011-02-24 16:00:17 -0600

    Combatants are about to get more complicated.

---
+++ lib/gurps_com_cal/combatant/base.rb
@@ -0,0 +1,47 @@
+module GurpsComCal
+  class Combatant
+    module Base
+      def initialize character
+        @character = character
+        @injury = 0
+      end
+
+      def method_missing method, *args, &block
+        if pass_to_character? method
+          @character.send(method, *args, &block)
+        else
+          super(method, *args, &block)
+        end
+      end
+
+      def damage type, basic_damage
+        injury = basic_damage * WOUNDING_MODS[type.to_s]
+        @injury += injury.ceil
+      end
+
+      def current_hp
+        @character.hp - @injury
+      end
+
+      private
+
+      def pass_to_character? method
+        @character.respond_to? method
+      end
+
+      WOUNDING_MODS = {
+        'pi-' => 0.5,
+        'burn' => 1.0,
+        'cor' => 1.0,
+        'cr' => 1.0,
+        'fat' => 1.0,
+        'pi' => 1.0,
+        'tox' => 1.0,
+        'cut' => 1.5,
+        'pi+' => 1.5,
+        'imp' => 2.0,
+        'pi++' => 2.0
+      }
+    end
+  end
+end
