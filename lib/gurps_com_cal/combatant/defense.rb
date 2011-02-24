766d2e6a42a2e79a3f76e5c66774455293f736b0
Ben Hamill <git-commits@benhamill.com>
2011-02-24 17:27:28 -0600

    List defenses.

--- lib/gurps_com_cal/combatant/defense.rb
+++ lib/gurps_com_cal/combatant/defense.rb
@@ -1,6 +1,22 @@
 module GurpsComCal
   class Combatant
     module Defense
+      POSSIBLE_DEFENSES = %w{parry dodge}
+      def defenses
+        POSSIBLE_DEFENSES.collect do |defense_name|
+          self.send("#{defense_name}?") ? defense_name : nil
+        end.compact
+      end
+
+      private
+
+      def dodge?
+        true
+      end
+
+      def parry?
+        equipped_weapon.respond_to(:parry)
+      end
     end
   end
 end
--- spec/combatant/defense_spec.rb
+++ spec/combatant/defense_spec.rb
@@ -5,7 +5,35 @@ class DefenseHolder
 end
 
 describe "GurpsComCal::Combatant::Defense" do
+  before(:each) do
+    @weapon = double(GurpsComCal::Weapon)
+    @weapon.stub(:respond_to).with(:parry) { false }
+    subject.stub(:equipped_weapon) { @weapon }
+  end
+
   subject { DefenseHolder.new }
 
-  pending
+  describe "#defenses" do
+    context "with an equipped melee weapon that can parry" do
+      before(:each) do
+        @weapon.stub(:respond_to).with(:parry) { true }
+      end
+
+      it "should list parry" do
+        subject.defenses.should include('parry')
+      end
+    end
+
+    context "without an equipped weapon that can parry" do
+      it "should NOT list parry" do
+        subject.defenses.should_not include('parry')
+      end
+    end
+
+    context "when the combatant can dodge" do
+      it "should list dodge" do
+        subject.defenses.should include('dodge')
+      end
+    end
+  end
 end
