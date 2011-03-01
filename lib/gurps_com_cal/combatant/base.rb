3694b96ebcdba4bd454fe3dee7f437bd779c8df0
Ben Hamill <git-commits@benhamill.com>
2011-03-01 02:03:46 -0600

    Proxy Combatant.load_yaml to Character.load_yaml.

--- lib/gurps_com_cal/combatant/base.rb
+++ lib/gurps_com_cal/combatant/base.rb
@@ -1,47 +1,60 @@
 module GurpsComCal
   class Combatant
     module Base
-      def initialize character
-        @character = character
-        @injury = 0
-      end
-
-      def method_missing method, *args, &block
-        if pass_to_character? method
-          @character.send(method, *args, &block)
-        else
-          super(method, *args, &block)
+      module ClassMethods
+        def load_yaml file_path
+          self.new(Character.load_yaml(file_path))
         end
       end
 
-      def damage type, basic_damage
-        injury = basic_damage * WOUNDING_MODS[type.to_s]
-        @injury += injury.ceil
+      module InstanceMethods
+        def initialize character
+          @character = character
+          @injury = 0
+        end
+
+        def method_missing method, *args, &block
+          if pass_to_character? method
+            @character.send(method, *args, &block)
+          else
+            super(method, *args, &block)
+          end
+        end
+
+        def damage type, basic_damage
+          injury = basic_damage * WOUNDING_MODS[type.to_s]
+          @injury += injury.ceil
+        end
+
+        def current_hp
+          @character.hp - @injury
+        end
+
+        private
+
+        def pass_to_character? method
+          @character.respond_to? method
+        end
+
+        WOUNDING_MODS = {
+          'pi-' => 0.5,
+          'burn' => 1.0,
+          'cor' => 1.0,
+          'cr' => 1.0,
+          'fat' => 1.0,
+          'pi' => 1.0,
+          'tox' => 1.0,
+          'cut' => 1.5,
+          'pi+' => 1.5,
+          'imp' => 2.0,
+          'pi++' => 2.0
+        }
       end
 
-      def current_hp
-        @character.hp - @injury
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
       end
-
-      private
-
-      def pass_to_character? method
-        @character.respond_to? method
-      end
-
-      WOUNDING_MODS = {
-        'pi-' => 0.5,
-        'burn' => 1.0,
-        'cor' => 1.0,
-        'cr' => 1.0,
-        'fat' => 1.0,
-        'pi' => 1.0,
-        'tox' => 1.0,
-        'cut' => 1.5,
-        'pi+' => 1.5,
-        'imp' => 2.0,
-        'pi++' => 2.0
-      }
     end
   end
 end
--- spec/combatant/base_spec.rb
+++ spec/combatant/base_spec.rb
@@ -1,48 +1,61 @@
 require_relative '../spec_helper'
 
 describe GurpsComCal::Combatant do
-  before(:each) do
-    @character = double()
-    @combatant = GurpsComCal::Combatant.new(@character)
-  end
+  context "class methods" do
+    subject { GurpsComCal::Combatant }
 
-  describe "proxying the character" do
-    it "should call character methods on the related character" do
-      @character.should_receive :st
-      @combatant.st
-    end
-
-    it "should NOT pass methods to the character that it won't resspond to" do
-      @character.stub(:respond_to?).with(:foo) { false }
-      @character.should_not_receive :foo
-      expect{@combatant.foo}.to raise_error(NoMethodError)
+    describe "yaml" do
+      it "should create a new character from the yaml" do
+        GurpsComCal::Character.should_receive(:load_yaml).with('path/to/character.yaml')
+        subject.load_yaml('path/to/character.yaml')
+      end
     end
   end
 
-  describe "damage" do
+  context "instance methods" do
     before(:each) do
-      @character.stub(:hp) { 10 }
+      @character = double()
+      @combatant = GurpsComCal::Combatant.new(@character)
     end
 
-    it "should compare injury to the character's HP to report current HP" do
-      @combatant.damage(:cr, 1)
-      @combatant.current_hp.should == 9
+    describe "proxying the character" do
+      it "should call character methods on the related character" do
+        @character.should_receive :st
+        @combatant.st
+      end
+
+      it "should NOT pass methods to the character that it won't resspond to" do
+        @character.stub(:respond_to?).with(:foo) { false }
+        @character.should_not_receive :foo
+        expect{@combatant.foo}.to raise_error(NoMethodError)
+      end
     end
 
-    it "should apply the wounding modifier to penetrating damage" do
-      @combatant.damage(:imp, 2)
-      @combatant.current_hp.should == 6
-    end
+    describe "damage" do
+      before(:each) do
+        @character.stub(:hp) { 10 }
+      end
 
-    it "should track injury over sucessive calls" do
-      @combatant.damage(:cr, 3)
-      @combatant.damage(:cr, 2)
-      @combatant.current_hp.should == 5
-    end
+      it "should compare injury to the character's HP to report current HP" do
+        @combatant.damage(:cr, 1)
+        @combatant.current_hp.should == 9
+      end
 
-    it "should NOT do fractions" do
-      @combatant.damage(:cut, 1)
-      @combatant.current_hp.should == 8
+      it "should apply the wounding modifier to penetrating damage" do
+        @combatant.damage(:imp, 2)
+        @combatant.current_hp.should == 6
+      end
+
+      it "should track injury over sucessive calls" do
+        @combatant.damage(:cr, 3)
+        @combatant.damage(:cr, 2)
+        @combatant.current_hp.should == 5
+      end
+
+      it "should NOT do fractions" do
+        @combatant.damage(:cut, 1)
+        @combatant.current_hp.should == 8
+      end
     end
   end
 end
