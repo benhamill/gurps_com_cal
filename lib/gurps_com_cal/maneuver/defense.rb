58b8a1ca8bc328fe54a86a3a376440b9e66e411b
Ben Hamill <git-commits@benhamill.com>
2011-02-28 21:16:27 -0600

    Green tests about dodging.

--- lib/gurps_com_cal/maneuver/defense.rb
+++ lib/gurps_com_cal/maneuver/defense.rb
@@ -14,7 +14,18 @@ module GurpsComCal
       end
 
       def selected_dodge
+        @next_method = :do_dodge
         @message = "#{@actor.name}, roll against #{@actor.dodge} and enter the result."
+      end
+
+      def do_dodge result
+        if result <= @actor.dodge
+          @message = 'A success!'
+          @state = 1
+        else
+          @message = 'A failure!'
+          @state = -1
+        end
       end
     end
   end
