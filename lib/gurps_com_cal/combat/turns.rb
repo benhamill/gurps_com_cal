99a9803a77f6560f24daa11815dfc0730cc15634
Ben Hamill <git-commits@benhamill.com>
2011-03-02 00:44:37 -0600

    Basic turn order decision.

--- lib/gurps_com_cal/combat/turns.rb
+++ lib/gurps_com_cal/combat/turns.rb
@@ -1,6 +1,11 @@
 module GurpsComCal
   class Combat
     module Turns
+      def turn_order
+        @turn_order ||= combatants.sort do |one, other|
+          -1 * combatant(one).basic_move <=> combatant(other).basic_move
+        end
+      end
     end
   end
 end
--- spec/combat/turns_spec.rb
+++ spec/combat/turns_spec.rb
@@ -8,6 +8,30 @@ describe "GurpsComCal::Combat::Turns" do
   subject { TurnsHolder.new }
 
   describe "#turn_order" do
-    pending
+    let(:the_flash) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+    let(:a_cheetah) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+    let(:some_schmoe) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+    let(:combatants) { {
+      'The Flash' => the_flash,
+      'A Cheetah' => a_cheetah,
+      'Some Schmoe' => some_schmoe,
+    } }
+
+    before(:each) do
+      subject.stub(:combatants) { combatants.keys }
+      subject.stub(:combatant) { |name| combatants[name] }
+    end
+
+    context "with combatants with different basic speeds" do
+      before(:each) do
+        the_flash.basic_move = 20
+        a_cheetah.basic_move = 10
+        some_schmoe.basic_move = 5
+      end
+
+      it "should return in order of basic move" do
+        subject.turn_order.should == ['The Flash', 'A Cheetah', 'Some Schmoe']
+      end
+    end
   end
 end
