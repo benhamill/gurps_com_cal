38ef83c28f5e111da4acbdb585a32106042a045f
Ben Hamill <git-commits@benhamill.com>
2011-03-01 23:21:51 -0600

    Some basic functionality.

--- lib/gurps_com_cal/combat/combatants.rb
+++ lib/gurps_com_cal/combat/combatants.rb
@@ -1,15 +1,21 @@
 module GurpsComCal
   class Combat
     module Combatants
-      module ClassMethods
+      def load_combatant file_name
+        combatant = Combatant.load_yaml(file_name)
       end
 
-      module InstanceMethods
+      def add_combatant combatant
+        @combatants ||= {}
+        @combatants[combatant.name] = combatant
       end
 
-      def self.included(receiver)
-        receiver.extend         ClassMethods
-        receiver.send :include, InstanceMethods
+      def combatants
+        @combatants.keys
+      end
+
+      def combatant name
+        @combatants[name]
       end
     end
   end
--- spec/combat/combatants_spec.rb
+++ spec/combat/combatants_spec.rb
@@ -5,5 +5,40 @@ class CombatantsHolder
 end
 
 describe "GurpsComCal::Combat::Combatants" do
-  pending
+  subject { CombatantsHolder.new }
+
+  describe "#load_combatant" do
+    it "should create a new combatant from a file" do
+      GurpsComCal::Combatant.should_receive(:load_yaml).with('path/to/file.yaml')
+      subject.load_combatant('path/to/file.yaml')
+    end
+  end
+
+  context "adding, listing and retreiving combatants" do
+    let(:rick) { double(GurpsComCal::Combatant, :name => 'Rick Castle') }
+    let(:thug) { double(GurpsComCal::Combatant, :name => 'Thug') }
+
+    before(:each) do
+      subject.add_combatant(rick)
+      subject.add_combatant(thug)
+    end
+
+    describe "add_combatant" do
+      it "should take a Combatant object" do
+        subject.instance_variable_get(:@combatants).should == { 'Rick Castle' => rick, 'Thug' => thug }
+      end
+    end
+
+    describe "combatants" do
+      it "should list all the combatants" do
+        subject.combatants.sort.should == ['Rick Castle', 'Thug'].sort
+      end
+    end
+
+    describe "combatant" do
+      it "should retrieve a combatant by name" do
+        subject.combatant('Rick Castle').should == rick
+      end
+    end
+  end
 end
