2af25477722e2061662017c7b6ba1448046fbfc0
Ben Hamill <git-commits@benhamill.com>
2011-03-03 01:21:32 -0600

    Move shit around in the tests.

--- spec/combat/turns_spec.rb
+++ spec/combat/turns_spec.rb
@@ -7,28 +7,26 @@ end
 describe "GurpsComCal::Combat::Turns" do
   subject { TurnsHolder.new }
 
+  let(:the_flash) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+  let(:a_cheetah) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+  let(:some_schmoe) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
+  let(:combatants) { {
+    'The Flash' => the_flash,
+    'A Cheetah' => a_cheetah,
+    'Some Schmoe' => some_schmoe,
+  } }
+
+  before(:each) do
+    the_flash.basic_move = 20
+    a_cheetah.basic_move = 10
+    some_schmoe.basic_move = 5
+
+    subject.stub(:combatants) { combatants.keys }
+    subject.stub(:combatant) { |name| combatants[name] }
+  end
+
   describe "#turn_order" do
-    let(:the_flash) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
-    let(:a_cheetah) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
-    let(:some_schmoe) { GurpsComCal::Combatant.new(GurpsComCal::Character.new) }
-    let(:combatants) { {
-      'The Flash' => the_flash,
-      'A Cheetah' => a_cheetah,
-      'Some Schmoe' => some_schmoe,
-    } }
-
-    before(:each) do
-      subject.stub(:combatants) { combatants.keys }
-      subject.stub(:combatant) { |name| combatants[name] }
-    end
-
     context "with combatants with different basic speeds" do
-      before(:each) do
-        the_flash.basic_move = 20
-        a_cheetah.basic_move = 10
-        some_schmoe.basic_move = 5
-      end
-
       it "should return in order of basic move" do
         subject.turn_order.should == ['The Flash', 'A Cheetah', 'Some Schmoe']
       end
@@ -36,6 +34,7 @@ describe "GurpsComCal::Combat::Turns" do
 
     context "with combatants with tied basic speeds but different DX" do
       before(:each) do
+        [the_flash, a_cheetah, some_schmoe].each { |c| c.basic_move = 5 }
         the_flash.dx = 5
         a_cheetah.dx = 6
         some_schmoe.dx = 7
@@ -47,6 +46,10 @@ describe "GurpsComCal::Combat::Turns" do
     end
 
     context "with combatants all tied" do
+      before(:each) do
+        [the_flash, a_cheetah, some_schmoe].each { |c| c.basic_move = 5 }
+      end
+
       it "should determine randomly" do
         subject.should_receive(:rand).exactly(3).times
         subject.turn_order
@@ -55,6 +58,10 @@ describe "GurpsComCal::Combat::Turns" do
   end
 
   describe "#next_turn" do
-    pending
+    context "before combat has started" do
+      it "should start combat with the first character in turn order" do
+        pending
+      end
+    end
   end
 end
