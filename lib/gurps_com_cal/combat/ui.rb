f4487851658f5230e7fdc34d48cc0f320e474c53
Ben Hamill <git-commits@benhamill.com>
2011-03-04 02:51:06 -0600

    Some UI stuff. One red test.

--- lib/gurps_com_cal/combat/ui.rb
+++ lib/gurps_com_cal/combat/ui.rb
@@ -1,6 +1,34 @@
 module GurpsComCal
   class Combat
     module UI
+      def say message
+        puts message
+      end
+
+      def menu options
+        options.each_with_index do |option, index|
+          say "#{index + 1}. #{option}"
+        end
+        input = ask "Selection:"
+
+        options[input.to_i - 1]
+      end
+
+      def ask message
+        loop do
+          input = gets(message)
+
+          case input
+          when 'exit'
+            exit
+          when 'load combatant'
+            file = ask 'Enter file name:'
+            self.load_combatant(file)
+          else
+            return input
+          end
+        end
+      end
     end
   end
 end
--- spec/combat/ui_spec.rb
+++ spec/combat/ui_spec.rb
@@ -7,5 +7,78 @@ end
 describe "GurpsComCal::Combat::UI" do
   subject { UIHolder.new }
 
-  pending
+  describe "#say" do
+    it "should print the message" do
+      subject.should_receive(:puts).with('Would you like to play a game?')
+      subject.say('Would you like to play a game?')
+    end
+  end
+
+  describe "#menu" do
+    before(:each) do
+      subject.stub(:ask) { "2" }
+      subject.stub(:say)
+    end
+
+    it "should print all the array elements with a number" do
+      subject.should_receive(:say).with("1. First option")
+      subject.should_receive(:say).with("2. Second option")
+      subject.should_receive(:say).with("3. Third option")
+      subject.menu(['First option', 'Second option', 'Third option'])
+    end
+
+    it "should ask for a selection" do
+      subject.should_receive(:ask).with('Selection:')
+      subject.menu(['First option', 'Second option', 'Third option'])
+    end
+
+    it "should return the selected option" do
+      subject.menu(['First option', 'Second option', 'Third option']).should == 'Second option'
+    end
+  end
+
+  describe "#ask" do
+    it "should use gets to gather input" do
+      subject.should_receive(:gets).with('foo')
+      subject.ask('foo')
+    end
+
+    it "should pass along gathered input" do
+      subject.stub(:gets) { '12' }
+      subject.ask('foo').should == '12'
+    end
+
+    context "when the 'exit' command is entered" do
+      before(:each) do
+        subject.stub(:gets) { 'exit' }
+      end
+
+      it "quit the program" do
+        lambda { subject.ask('What is your favorite color?') }.should raise_error SystemExit
+      end
+    end
+
+    context "when the 'load combatant' command is entered" do
+      before(:each) do
+        subject.stub(:gets).with('What is your favorite color?').and_return('load combatant', '12')
+        subject.stub(:gets).with('Enter file name:') { 'path/to/file.yaml' }
+        subject.stub(:load_combatant)
+      end
+
+      it "should ask for the file path" do
+        subject.should_receive(:gets).with('Enter file name:')
+        subject.ask('What is your favorite color?')
+      end
+
+      it "should load the combatant" do
+        subject.should_receive(:load_combatant).with('path/to/file.yaml')
+        subject.ask('What is your favorite color?')
+      end
+
+      it "should ask for the other input when it's done" do
+        subject.should_receive(:gets).with('What is your favorite color?').exactly(2).times
+        subject.ask('What is your favorite color?')
+      end
+    end
+  end
 end
