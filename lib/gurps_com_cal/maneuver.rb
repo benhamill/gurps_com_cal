921bc21e97e9acab1b30d6db072ed68fa5286df0
Ben Hamill <git-commits@benhamill.com>
2011-03-03 23:39:40 -0600

    Gonna want this later.

--- lib/gurps_com_cal/maneuver.rb
+++ lib/gurps_com_cal/maneuver.rb
@@ -1,3 +1,12 @@
 require_relative 'maneuver/base'
-require_relative 'maneuver/attack'
 require_relative 'maneuver/defense'
+
+require_relative 'maneuver/attack'
+
+module GurpsComCal
+  module Maneuver
+    LIST = [
+      "Attack",
+    ]
+  end
+end
